e1e6c94ec339d49603ebeb504d82f6929ce28991
boris <boris.alekhno@gmail.com>
2023-09-21 10:29:28 +0300

    fixed test

--- src/test/java/com/abd/example/integrationtest/HelloControllerIntegrationTest.java
+++ src/test/java/com/abd/example/integrationtest/HelloControllerIntegrationTest.java
@@ -2,11 +2,13 @@ package com.abd.example.integrationtest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerIntegrationTest {
 
     @Autowired
